fe36e05ade0685ed5561788dd4b45227afd3b120
Houmadi Hairou <hairou.houmadi@gmail.com>
2025-08-20 10:43:13 +0200

    save activite

--- mon_portfolio/src/pages/Homepage.jsx
+++ mon_portfolio/src/pages/Homepage.jsx
@@ -1,8 +1,8 @@
-
+import '../SASS/pages/homepage.css'
 
 function Homepage () {
     return(
-        <section>
+        <section className="Homepage">
             <div id="Presenta" className="presentation">
                 <h2>Présentation</h2>
                 <p>
@@ -17,7 +17,60 @@ function Homepage () {
             </div>
             <div id="activites" className="activites">
                 <h2>Activités</h2>
-
+                <div className="act-container">
+                    <div className="git-container">
+                        <p>Dernier dépôts GitHub récemment mis à jour</p>
+                        <div className="Act-Bank">
+                            <a 
+                            href="https://github.com/Hai-rou/Argent-Bank"
+                            target="_blank"
+                            rel="noolpener noreferrer"
+                            className="Bank-btn">
+                            <img src="./src/assets/argentBankLogo.webp" alt="Image projet Argent Bank" />
+                            Projet Argent Bank
+                            </a>
+                            <a 
+                            href="https://github.com/Hai-rou/Projet_9-Debuggez-une-application-React.js"
+                            target='_blank'
+                            rel='noolpener noreferrer'
+                            className='events-btn'>
+                            <img src="./src/assets/724.webp" alt="Logo projet 724 debuggage" />
+                            724 Events
+                            </a>
+                            <a 
+                            href="https://github.com/Hai-rou/Projet_6"
+                            target='_blank'
+                            rel='noolpener noreferrer'
+                            className='sophie-btn'>
+                            <img src="./src/assets/sophie.webp" alt="Logo projet Sophie Bluel" />
+                            Sophie Bluel
+                            </a>
+                            <a href="https://github.com/Hai-rou/Projet_8-Nina_Carducci"
+                            target='_blank'
+                            rel='noolpener noreferrer'
+                            className='nina-btn'
+                            >
+                            <img src="./src/assets/nina.webp" alt="Logo projet Nina Carducci" />
+                            Nina Carducci
+                            </a>
+                        </div>
+                    </div>
+                    <div className='diplo-container'>
+                        <p>Dernier diplôme, emploi et projet</p>
+                        <div className='item'>
+                            <img src="./src/assets/openclassrooms.webp" alt="Logo Openclassrooms" />
+                            <div className='context'>
+                                <h3>Intégrateur Web</h3>
+                                <p>OpenClassrooms</p>
+                            </div>
+                            <p>Formations</p>
+                            <p>2025</p>
+                        </div>
+                    </div>
+                    <div className='reseau-container'>
+                        <p>Connectez-vous avec moi</p>
+                    </div>
+                </div>    
             </div>
             <div id="competences" className="competences">
                 <h2>Compétences</h2>
